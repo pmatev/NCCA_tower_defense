5211e1b19de062c0d1b62bd2b47c0523e6e95794
Jared Auty <i7995054@wg1028>
2012-11-21 15:40:58 +0000

    Added some handy scripts for pushing and pulling

---
+++ scripts/pull.sh
@@ -0,0 +1,1 @@
+bzr pull http://aberdeen.bournemouth.ac.uk/srv/repos/bzr/BA3TowerDefence
---
+++ scripts/push.sh
@@ -0,0 +1,2 @@
+echo rfrazabber
+bzr push sftp://cva3student@aberdeen.bournemouth.ac.uk/srv/repos/bzr/BA3TowerDefence
