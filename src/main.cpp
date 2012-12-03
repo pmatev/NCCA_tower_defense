4ef3ba80cec82afdd2270dff7a1696faefbcf486
Peter David May <i7935026@wg1022>
2012-12-03 19:19:53 +0000

    changed the database class to a singleton, changed entity record to a struct within the database class and added a clear records method to the database

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,17 @@
 
 #include "window.h"
 
-
-#include "entityfactory.h"
-#include "enemy.h"
-
 int main(int argc, char *argv[])
 {
-    // create and init the OpenGL Window
-    WindowPtr w = Window::create();
-    w->init();
+  // create and init the OpenGL Window
+  WindowPtr w = Window::create();
+  w->init();
 
-    // start the loop
-    w->loop();
+  // start the loop
+  w->loop();
 
-    //exit and close
-    w->quit();
+  //exit and close
+  w->quit();
 }
 
 
